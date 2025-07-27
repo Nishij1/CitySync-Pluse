f83ab2815aa035c7d8f89b3b38bd40208a74d2d0
Nishij Sontakke <nishijsontakke000@gmail.com>
2025-07-27 10:39:21 +0530

    v4.1

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'citysync-pluse',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'citysync-pluse',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
